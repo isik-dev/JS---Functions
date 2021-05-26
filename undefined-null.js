5e39cda359a9d6eff7afa7f0dd64ec9ebbda8a20
islomnumanov <islom4ik.numanov@gmail.com>
2021-05-26 21:38:12 +0900

    undefined-null

---
+++ undefined-null.js
@@ -0,0 +1,28 @@
+// Undefined for variable - create a variable and not assigning any value to it
+let name
+
+name = 'Jen'
+
+if (name === undefined) {
+    console.log('Please provide a name')
+} else {
+    console.log(name)
+}
+
+// Undefined for function arguments
+// Undefined as function return default value
+
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square()
+
+console.log(result)
+
+// Null as assigned value
+let age = 27
+
+age = null
+
+console.log(age)
